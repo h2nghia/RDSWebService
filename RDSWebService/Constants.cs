5945a6f649a4cc1fc8676e0c5e90959e80e739c2
nilehuynh <9nghia@gmail.com>
2015-04-14 10:28:47 -0700

    Add constant file without server information.

--- RDSWebService/Constants.cs
+++ RDSWebService/Constants.cs
@@ -2,10 +2,9 @@
 {
     public static class Constants
     {
-        //public const string DB_CONNECTION_STRING = "Server=CHRIS-DESKTOP;Database=RDSWebService;MultipleActiveResultSets=true;User Id=RDSWebService;Password=flattire77";
-        public const string DB_CONNECTION_STRING = "Server=RDSWEBAPP\\sqlexpress;Database=RDSWebService;MultipleActiveResultSets=true;User Id=RDSWebService;Password=flattire";
-
-        public const string AUTHORIZATION_USER_NAME = "burnrubber";
-        public const string AUTHORIZATION_PASSWORD = "flattire77";
+        public const string DB_CONNECTION_STRING = "ConnectionString";
+        
+        public const string AUTHORIZATION_USER_NAME = "username";
+        public const string AUTHORIZATION_PASSWORD = "password";
     }
 }
